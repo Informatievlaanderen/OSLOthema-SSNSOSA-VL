6ec466f0c824a50855a116fb412bdea620819b11
gezever <gezever@skynet.be>
2026-02-26 11:54:10 +0100

    mkdir -p

--- resources/EnergieManagementSystem/bash/getSource.sh
+++ resources/EnergieManagementSystem/bash/getSource.sh
@@ -7,7 +7,7 @@ curl -O https://raw.githubusercontent.com/Informatievlaanderen/OSLOthema-Energie
 curl -O https://raw.githubusercontent.com/Informatievlaanderen/OSLOthema-EnergieManagementSystem/refs/heads/main/resources/datavoorbeelden/DatavoorbeeldHoofdaansluitingSubaansluiting.json
 
 for i in *json ; do
-  [[ -d ../${i/.json/}/source ]] || mkdir ../${i/.json/}/source
+  [[ -d ../${i/.json/}/source ]] || mkdir -p ../${i/.json/}/source
   mv $i ../${i/.json/}/source/${i/json/jsonld} ;
   riot --formatted=turtle prefixen.ttl ../${i/.json/}/source/${i/json/jsonld} > ../${i/.json/}/source/${i/json/ttl} ;
 done
