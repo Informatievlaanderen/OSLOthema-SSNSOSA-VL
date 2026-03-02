91dc8a2ab926719352d58351b63d4e080f46af16
gezever <gezever@skynet.be>
2026-03-02 18:09:52 +0100

    shacl validatie

--- src/main/input/grondwaterpeilmetingen/transform.sh
+++ src/main/input/grondwaterpeilmetingen/transform.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 xsltproc transformatie.xslt source.xml > grondwaterpeilmetingen.rdf
-#riot --output=turtle /home/gehau/git/OSLOthema-SSNSOSA-VL/src/main/input/grondwaterpeilmetingen/grondwaterpeilmetingen.rdf | sed -e 's/_:/ex:/g' > /tmp/grondwaterpeilmetingen.ttl
-#riot --formatted=turtle /tmp/grondwaterpeilmetingen.ttl > grondwaterpeilmetingen.ttl
 riot --formatted=turtle grondwaterpeilmetingen.rdf > grondwaterpeilmetingen.ttl
 sparql --query query.sparql --data=grondwaterpeilmetingen.ttl --results=CSV > grondwaterpeilmetingen.csv
