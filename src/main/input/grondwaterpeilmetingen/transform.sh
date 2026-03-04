bb70071ccf869290f73b73fe8a9af28b21a7713e
gezever <gezever@skynet.be>
2026-03-04 17:57:48 +0100

    grondwaterpeiling

--- src/main/input/grondwaterpeilmetingen/transform.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-xsltproc transformatie.xslt source.xml > /tmp/grondwaterpeilmetingen.rdf
-riot --formatted=trig /tmp/grondwaterpeilmetingen.rdf > grondwaterpeilmetingen.trig
-rm /tmp/grondwaterpeilmetingen.rdf
-sparql --query query.sparql --data=grondwaterpeilmetingen.trig --results=CSV > grondwaterpeilmetingen.csv
